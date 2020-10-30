56a09f5886faf536a200aa43b03e6399e5e15e5d
Ophir Yael <ofir.yael@gmail.com>
2020-10-30 22:59:36 +0200

    Add span table creation migration

---
+++ src/db/migrations/20201030223833_create_span_table.ts
@@ -0,0 +1,15 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('span', (table: Knex.TableBuilder) => {
+    table.string('id').notNullable().primary();
+    table.string('parent_id').references('span.id');
+    table.string('operation_name').notNullable();
+    table.timestamp('start_time').notNullable();
+    table.integer('duration').notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('span');
+}
