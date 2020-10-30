c174d6247c7a53696718f447e4eb6a302d2d22d3
Ophir Yael <ofir.yael@gmail.com>
2020-10-30 23:39:19 +0200

    Add log table creation migration

---
+++ src/db/migrations/20201030232816_create_log_table.ts
@@ -0,0 +1,17 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('log', (table: Knex.TableBuilder) => {
+    table.uuid('id').notNullable().primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('span_id').notNullable().references('span.id');
+    table.timestamp('timestamp').notNullable();
+    table.string('key').notNullable();
+    table.string('string_value');
+    table.decimal('numeric_value');
+    table.boolean('boolean_value');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('log');
+}
