33f3111d2963c2661b278a3b87e125fda829ac43
Ophir Yael <ofir.yael@gmail.com>
2020-10-30 22:09:42 +0200

    Add knex connection config

---
+++ src/connection.ts
@@ -0,0 +1,27 @@
+import Knex from 'knex';
+import path from 'path';
+
+function createConfig() {
+  return {
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: getMigrationsDir(),
+    },
+    seeds: {
+      directory: getSeedsDir(),
+      extension: 'ts',
+    },
+  };
+}
+
+function getMigrationsDir(): string {
+  return path.join(__dirname, 'migrations');
+}
+
+function getSeedsDir(): string {
+  return path.join(__dirname, 'seeds');
+}
+
+export const dbKnex: Knex = Knex(createConfig());
