078c5507d16cd8479c602740126325d484967f1c
Ophir Yael <ofir.yael@gmail.com>
2020-11-02 16:03:01 +0200

    Add cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { spansRouter } from './routes/spans';
 
 export function runServer(): void {
   const app = express();
+
+  app.use(cors());
   app.use(express.json());
 
   app.use('/spans', spansRouter);
